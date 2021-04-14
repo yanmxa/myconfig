effc133f024af7f023af128072fffed85203fae6
Meng Yan <yanmxa@163.com>
2021-04-14 17:24:57 +0800

    z

--- serv.sh
+++ serv.sh
@@ -2,5 +2,5 @@
 echo "add login server enviroment"
 
 CURRENT_DIR=`pwd`
-echo "alias serv=sh $CURRENT_DIR/server/serv.sh" >> ~/.bash_profile
+echo "alias serv=sh $CURRENT_DIR/server/login.sh" >> ~/.bash_profile
 source ~/.bash_profile
