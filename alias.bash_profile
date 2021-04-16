64d2a36283189f283fc6aad1c223f87136143d4b
Meng Yan <yanmxa@163.com>
2021-04-16 11:12:10 +0800

    add alias and auth

---
+++ alias.bash_profile
@@ -0,0 +1,18 @@
+alias vi=vim
+alias vim=mvim
+alias mvim='/usr/local/bin/mvim -v'
+alias serv='sh /Users/yanmeng/workspace/other/myconfig/server/login.sh'
+alias load='sh /Users/yanmeng/workspace/other/myconfig/server/upload.sh'
+alias auth='sh /Users/yanmeng/workspace/other/myconfig/server/copy-ssh-id_rsa.pub-to-authorized_keys.sh'
+alias lime='open -a /Applications/Sublime\ Text.app'
+alias bai='open -a /Applications/Google\ Chrome.app https://www.baidu.com/s\?wd\=\'
+alias tran='open -a /Applications/Google\ Chrome.app https://translate.google.cn/'
+alias mail='open -a MailMaster.app'
+alias note='code /Users/yanmeng/workspace/notes'
+alias google='open -a /Applications/Google\ Chrome.app'
+alias github='open -a /Applications/Google\ Chrome.app https://github.com'
+alias we='open -a WeChat.app'
+alias bear='open -a Bear.app'
+alias postman='open -a Postman.app'
+alias idea='open -a IntelliJ\ IDEA.app'
+alias ding='open -a DingTalk.app'
---
+++ server/copy-ssh-id_rsa.pub-to-authorized_keys.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+##############################################################################################
+### create by : Meng Yan
+### create time : 20210414
+### usage : used to login service 
+### arguments : 
+### 	hosts.txt: name server password port
+###     $1: file or dir to be uploaded
+###     $2: target path 
+### dependency : with middle virtual machine serv.exp
+##############################################################################################
+names=()
+servers=()
+passwords=()
+ports=()
+
+while read line 
+do
+	name=`echo $line | awk '{print $1}'`
+	serv=`echo $line | awk '{print $2}'`
+	pass=`echo $line | awk -F" " '{print $3}'`
+	port=`echo $line | awk -F" " '{print $4}'`
+	if test -z "$port"
+	then
+		port='22'
+	fi
+	names=(${names[@]} $name)
+	servers=(${servers[@]} $serv)
+	passwords=(${passwords[@]} $pass)
+	ports=(${ports[@]} $port)
+done<"`dirname $0`/host.txt"
+
+select option in ${names[@]}
+do
+	select_name=${option}
+    break;
+done
+
+for index in "${!names[@]}"
+do
+	if [[ $select_name = ${names[index]} ]]
+	then 
+		break
+	fi
+done
+
+if [[ $select_name == '' ]]; then
+	echo ">> no server selected!"
+	exit 0
+fi
+
+
+# echo name ${names[index]}
+# echo servers ${servers[index]}
+# echo port ${ports[index]}
+# echo pass ${passwords[index]}
+echo ">> upload to ${names[index]}"
+expect -c 'spawn ssh-copy-id '"-p ${ports[index]} ${servers[index]}"'; expect "*assword*"; send '"${passwords[index]}\r"'; interact'
