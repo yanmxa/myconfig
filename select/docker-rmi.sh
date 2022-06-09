a0f0cf66e4d4dfd214b3f92a917d57a552964568
myan <myan@redhat.com>
2022-06-09 15:11:58 +0800

    fixed index issue

--- select/docker-rmi.sh
+++ select/docker-rmi.sh
@@ -32,6 +32,7 @@ for (( i = 0; i < ${#images[@]}; i++ )); do
 done
 read -t 50 -p "#? " index
 echo "docker rmi ${image_names[$index]}:${image_tags[$index]}"
-docker rmi "${image_names[$i]}:${image_tags[$i]}"
-
+docker rmi ${image_names[$index]}:${image_tags[$index]}
+# echo "docker rmi ${image_ids[$index]}"
+# docker rmi "${image_ids[$index]}"
 
