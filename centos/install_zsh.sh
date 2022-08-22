6f559d6a232624a86eec33cac2005454990900cc
Meng <yanmxa@163.com>
2022-08-22 17:50:47 +0800

    zsh
    Signed-off-by: Meng <yanmxa@163.com>

--- centos/install_zsh.sh
+++ centos/install_zsh.sh
@@ -7,23 +7,7 @@ sudo dnf install util-linux-user -y
 sudo chsh -s $(which zsh)
 
 
-# oh-my-zsh
-sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp ~/.zshrc ~/.zshrc.orig
 
-# autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-git clone https://github.com/joelthelion/autojump.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump
-cd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump
-python3 install.py
-
-currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp $currentDir/.zshrc ~/.zshrc
-source ~/.zshrc
 
 
 
