9fa2ff585c07ebb38052440e1eb64d6463eb067b
myan <myan@redhat.com>
2023-09-21 19:57:22 +0800

    add python installation
    Signed-off-by: myan <myan@redhat.com>

--- lang/install_python.sh
+++ lang/install_python.sh
@@ -1,5 +1,10 @@
-dnf install python3
-# 查看python文件路径
-whereis python 
-# 做个软链接 
-ln -s /usr/bin/python3.6 /usr/bin/python 
+yum install gcc yum-utils zlib-devel python-tools cmake git pkgconfig -y --skip-broken
+yum groupinstall -y "Development Tools" --skip-broken
+
+wget https://www.python.org/ftp/python/3.11.3/Python-3.11.3.tgz
+tar xzf Python-3.11.3.tgz
+./configure
+
+make
+make install
+python3 --version
