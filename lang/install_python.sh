f7df40e7cb64762c49187649762c3ab8383fa8c2
myan <myan@redhat.com>
2023-09-21 20:44:36 +0800

    python dependency
    Signed-off-by: myan <myan@redhat.com>

--- lang/install_python.sh
+++ lang/install_python.sh
@@ -1,6 +1,8 @@
 yum install gcc yum-utils zlib-devel python-tools cmake git pkgconfig -y --skip-broken
 yum groupinstall -y "Development Tools" --skip-broken
 
+yum install libffi-devel bzip2-devel -y
+
 wget https://www.python.org/ftp/python/3.11.3/Python-3.11.3.tgz
 tar xzf Python-3.11.3.tgz
 ./configure
