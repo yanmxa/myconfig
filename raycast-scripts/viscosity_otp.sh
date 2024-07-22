6f50bada871e4558bfa97d9c670b02e3c79e7bb9
myan <myan@redhat.com>
2024-07-22 17:33:26 +0800

    add raycast script
    Signed-off-by: myan <myan@redhat.com>

---
+++ raycast-scripts/viscosity_otp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Connect Viscosity
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon images/viscosity.png
+# @raycast.argument1 { "type": "dropdown", "placeholder": "GLOBAL/PEK2", "data": [{"title": "GLOBAL", "value": "Red Hat Global VPN (TCP Alternate)"}, {"title": "PEK2", "value": "Beijing (PEK2 Alternate)"}] }
+# @raycast.packageName Developer Utils
+# @raycast.needsConfirmation false
+
+# Documentation:
+# @raycast.author Meng Yan
+# @raycast.authorURL https://www.raycast.com/yanmxa
+
+# 1. the otp command is from here: https://www.npmjs.com/package/awesome-otp
+# 2. init the vpn secret: otp init
+# 3. create/add the prefix/password to a file `echo "password" > ~/.otp_prefix`
+
+name="$1"
+if [ -z "$name" ]; then
+    name="Red Hat Global VPN (TCP Alternate)"
+fi
+
+otp token --prefix "$(cat ~/.otp_prefix)" --no-info && otp viscosity -o "$name"
